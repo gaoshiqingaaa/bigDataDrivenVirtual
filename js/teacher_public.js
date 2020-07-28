d66a936ae703680716792c1a2147e256962cbcbe
apeic <648870158@qq.com>
2020-07-28 15:27:37 +0800

    add some bg

--- js/teacher_public.js
+++ js/teacher_public.js
@@ -11,7 +11,7 @@ var code_in = CodeMirror.fromTextArea(document.getElementById("code-in"), {
     //readOnly: true,        //只读
 });
 
-code_in.setSize('550px','570px'); //宽 高
+code_in.setSize('500px','570px'); //宽 高
 
 var code_out = CodeMirror.fromTextArea(document.getElementById("code-out"), {
     mode: {
