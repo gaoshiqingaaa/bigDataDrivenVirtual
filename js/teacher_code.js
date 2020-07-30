175d2a0c0000784ba36afc3834aa73ceb460e4a5
apeic <648870158@qq.com>
2020-07-30 17:04:36 +0800

    add teacher index.html

--- js/teacher_code.js
+++ js/teacher_code.js
@@ -2,7 +2,7 @@ var code_in = CodeMirror.fromTextArea(document.getElementById("code-in"), {
     mode: {
         name: 'python'
     },
-    // lineNumbers: true,
+    lineNumbers: true,
     theme: "idea",	//设置主题
     lineWrapping: true,	//代码折叠
     foldGutter: true,
@@ -11,6 +11,21 @@ var code_in = CodeMirror.fromTextArea(document.getElementById("code-in"), {
     //readOnly: true,        //只读
 });
 code_in.setSize('500px','570px'); //宽 高
+
+var code_out = CodeMirror.fromTextArea(document.getElementById("code-out"), {
+    mode: {
+        name: 'python'
+    },
+    // lineNumbers: true,
+    theme: "idea",	//设置主题
+    lineWrapping: true,	//代码折叠
+    foldGutter: true,
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+    matchBrackets: true,	//括号匹配
+    readOnly: true,        //只读
+});
+code_out.setSize('500px','570px'); //宽 高
+
 
 $('.run-code').click(function(){
     $.ajax({
@@ -22,13 +37,8 @@ $('.run-code').click(function(){
         },
         success: function(data){
             $('.result').empty()
-            if (data.result != ''){
-                html = data.result.split('\n').map(a=>'<p>' + a + '</p>').join('\n')
-                
-            } else if (data.error != '') {
-                html = data.error.split('\n').map(a=>'<p>' + a + '</p>').join('\n')
-            }
-            $('.result').append(html)
+            result = data.error == ''? data.result: data.error
+            code_out.setValue(result)
         }   
     })
 })
